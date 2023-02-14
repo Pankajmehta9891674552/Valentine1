5e7b694bf18ccd01c693392b764a14f00014d407
Sam Smith <pankaj.mehta989@gmail.com>
2023-02-14 14:26:47 +0530

    Valentine first comment

---
+++ Hi/src/Hi1.java
@@ -0,0 +1,12 @@
+
+public class Hi1 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hi1calling");
+		
+	}
+	
+}
---
+++ Hi/src/Hi2.java
@@ -0,0 +1,10 @@
+
+public class Hi2 
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hi2calling");
+		
+	}
+}
